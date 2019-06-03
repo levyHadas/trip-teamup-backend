db1f35eee3d0a65b1881dd8e4e06444cbbb82674
levy Hadas <hadasl789@gmail.com>
2019-06-03 17:14:36 +0300

    build folder in backend

---
+++ build/precache-manifest.aac85050f75948f92c8d6594a6c83bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b858ee9e5c0e284ec0b6d8dc2bf07e3c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "272b98cd0b2cc6edf429",
+    "url": "/static/css/main.edd89905.chunk.css"
+  },
+  {
+    "revision": "c5e7cb6ee297e038c638",
+    "url": "/static/js/2.01f1b2e6.chunk.js"
+  },
+  {
+    "revision": "272b98cd0b2cc6edf429",
+    "url": "/static/js/main.8ec5dcc9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a6b250cc75de9ad9e2ec40ad3823491",
+    "url": "/static/media/home-bg2.4a6b250c.jpg"
+  }
+]);
