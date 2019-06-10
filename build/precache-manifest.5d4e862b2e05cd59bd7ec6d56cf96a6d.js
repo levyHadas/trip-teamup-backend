1c46cb8593b9cb27f894434d2cd4409f662b456e
levy Hadas <hadasl789@gmail.com>
2019-06-10 19:36:27 +0300

    After build

---
+++ build/precache-manifest.5d4e862b2e05cd59bd7ec6d56cf96a6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51095a8f1b2a10351e51c8c9ef55716b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0bcc65efd206f6c0e483",
+    "url": "/static/css/main.78cd1c5a.chunk.css"
+  },
+  {
+    "revision": "bdbc618d59642e0df37d",
+    "url": "/static/js/2.ec13de37.chunk.js"
+  },
+  {
+    "revision": "0bcc65efd206f6c0e483",
+    "url": "/static/js/main.f331a157.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "4a6b250cc75de9ad9e2ec40ad3823491",
+    "url": "/static/media/home-bg2.4a6b250c.jpg"
+  },
+  {
+    "revision": "9e36fbb02f925ed1b4242a2359867457",
+    "url": "/static/media/logo.9e36fbb0.png"
+  }
+]);
